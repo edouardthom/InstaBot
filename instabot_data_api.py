3072222d1b0aedb5cefb0f2ad38c8f432d4c9e7e
Edouard Thomas <edouard.thomas@booking.com>
2020-11-17 02:12:02 +0100

    introduction of the dataAPI

--- instabot_data_api.py
+++ instabot_data_api.py
@@ -15,16 +15,18 @@
 
 ###### 2. How to use it :
 ### - Register a new table definition :
-# in the class registeredTableDefinitions, create a new class variable. The name of the variable is the 
+# In the class registeredTableDefinitions, create a new class variable. The name of the variable is the 
 # name of the table definition. The value of the variable has to be a dictionary with the 
 # following structure : {"description" : "Table description", "schema":{"col_1_name":"description",...}}
 # There can be several tables (CSVs) following a given table definition : one per user.
+# If a table follows the table definition X and stores the data for user joebiden, its name will be 
+# X_joebiden, and it will be stored into the CSV file "X_joebiden.csv".
 # A table cannot be managed by the API if it doesn't follow a registered table definition.
 ### - Call the API
 # Once a table definition has been registered, there can be as many tables (CSVs) following that table definition
 # as users. 
 # You can : 
-# - load these tables as pandas dataframe : dataAPI().get(table_definition_name",user_name)
+# - load these tables as pandas dataframe : dataAPI().get(table_definition_name,user_name)
 # - add a record to any table : dataAPI().get(table_definition_name",user_name,record)
 # - store a new version of the table : dataAPI().store(table_definition_name",user_name,new_table)
 # You can see that for each call to the API you have to specify an existing table_definition_name
@@ -35,7 +37,15 @@ import os
 import pandas as pd
 
 
-class registeredTablesDefinitions:
+class registeredTableDefinitions:
+    
+    '''
+    In that class all the table definitions, stored as class variables.
+    The name of the class variable is the name of the table definition.
+    A table definitions is a table description, a list of column names and a name for each column .
+    It is stored under the form of the dict with the following structure :
+        {"description" : "Table description", "schema":{"col_1_name":"description",...}}
+    '''
     
     follow_actions = {
     "description" : "1 new row each time the bot follows an account",
@@ -60,19 +70,19 @@ class registeredTablesDefinitions:
     }}
 
 
-class dataAPI(registeredTablesDefinitions):
+class dataAPI(registeredTableDefinitions):
     
     def __init__(self):
         pass
         
     def get(self,name,user):
-        if name not in [i for i in dir(registeredTablesDefinitions) if not callable(i)]:
+        if name not in [i for i in dir(registeredTableDefinitions) if not callable(i)]:
             raise Exception("DataAPITableDoesntExist")
         exists = os.path.isfile(name+"_"+user+".csv")
         if exists:
             data = pd.read_csv(name+"_"+user+".csv")
         else:
-            schema = getattr(registeredTablesDefinitions, name).get("schema")
+            schema = getattr(registeredTableDefinitions, name).get("schema")
             columns = list(schema.keys())
             data = pd.DataFrame(columns = columns)
         return data  
@@ -80,7 +90,7 @@ class dataAPI(registeredTablesDefinitions):
     def add_record(self,name,user,record):
         if type(record) != dict:
             raise Exception("DataAPIInvalidRecord") 
-        schema = getattr(registeredTablesDefinitions, name).get("schema")
+        schema = getattr(registeredTableDefinitions, name).get("schema")
         schema_columns = list(schema.keys())
         record_columns = list(record.keys())
         record_ok = set(record_columns).issubset(set(schema_columns))
@@ -92,7 +102,7 @@ class dataAPI(registeredTablesDefinitions):
             raise Exception("DataAPIInvalidRecord") 
         
     def store(self,name,user,data):
-        schema = getattr(registeredTablesDefinitions, name).get("schema")
+        schema = getattr(registeredTableDefinitions, name).get("schema")
         schema_columns = list(schema.keys())
         data_ok = set(data.columns).issubset(set(schema_columns))
         if data_ok:
